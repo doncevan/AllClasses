4d82030160da0e41be75c1263b363535164a9862
doncevan <doncevan@gmail.com>
2023-05-07 16:20:18 -0400

    class18

---
+++ src/class18/A.java
@@ -0,0 +1,13 @@
+package class18;
+
+public class A {
+
+}
+
+class B extends A {
+
+}
+
+class C extends B {
+
+}
---
+++ src/class18/AnimalTester.java
@@ -0,0 +1,16 @@
+package class18;
+
+public class AnimalTester {
+    public static void main(String[] args) {
+        /*
+    create 5 classes of different animals like Cat,Dog ,Zebra,Camel,Horse etc
+    in each class create 5 fields and 3 methods.
+     */
+
+        Horse horse = new Horse();
+        horse.color = "Black";
+        System.out.println(horse.color = "Black");
+        horse.eat();
+
+    }
+}
