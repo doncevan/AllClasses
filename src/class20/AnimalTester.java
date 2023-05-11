d6eee2ed9c64de5c1caec8c500fd1da5007b5905
doncevan <doncevan@gmail.com>
2023-05-10 21:52:23 -0400

    class20

---
+++ src/class20/AnimalTester.java
@@ -0,0 +1,25 @@
+package class20;
+
+public class AnimalTester {
+    public static void main(String[] args) {
+
+        Dog dog=new Dog("Jam","Brown",10);
+        Cat cat=new Cat("Tom","White",5);
+        Horse horse=new Horse("Jam","Brown",10);
+
+
+        dog.sleep();
+     /*   dog.eat();
+        dog.sleep();
+        dog.speak();
+
+        cat.eat();
+        cat.sleep();
+        cat.speak();
+
+        horse.eat();
+        horse.sleep();
+        horse.speak();*/
+
+    }
+}
